efe57bb72edc34e3af1900372a9123186c3b3e96
DIEGOSANOLI <diego.sanoli@gmail.com>
2026-03-30 14:41:48 -0300

    Desafio Resolvido

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Missão 1.1
+
+string[] pocoes = { "Cura", "Mana" };
+
+if (pocoes.Length > 0)
+{
+    Console.WriteLine("Poção consumida!");
+}
+else
+{
+    Console.WriteLine("Inventário Vazio!");
+}
+
+
+// Missão 1.2
+
+int nivelJogador = 55;
+
+if (nivelJogador >= 50)
+{
+    Console.WriteLine("Porta VIP liberada");
+}
+else
+{
+    Console.WriteLine("Vá para treinamento!");
+}
+
+// Missão 1.3
+
+int noAtual = 50;
+int valorProcurado = 85;
+
+if (valorProcurado > noAtual)
+{
+    Console.WriteLine("Ir para DIREITA");
+}
+else
+{
+    Console.WriteLine("Ir para ESQUERDA");
+}
+
